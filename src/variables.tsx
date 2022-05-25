d70e6bf6d6fe86ff15a6d7ec2f5f0489a3221def
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-05-25 17:42:40 +0900

    Add validation to join form
    - Added checking email with regex.
    - Added checking password with regex.
    - Removed required attribute from phone input .

--- src/variables.tsx
+++ src/variables.tsx
@@ -1,2 +1,4 @@
 export const SERVER_URL = 'http://3.37.234.117:5000';
 export const LOCAL_URL = 'http://localhost:5000';
+export const pwRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?~^<>,.&+=])[A-Za-z\d$@$!%*#?~^<>,.&+=]{8,15}$/;
+export const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*))@((([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
